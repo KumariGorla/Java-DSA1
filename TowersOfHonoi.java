185f07459332058c1e69e2582aca3aa5ae596a54
kumarigorla <kumarigorla09@gmail.com>
2026-03-25 11:45:55 +0530

    Updated last two lines in Towers of Hanoi

--- TowersOfHonoi.java
+++ TowersOfHonoi.java
@@ -10,5 +10,7 @@ public class TowersOfHonoi {
     }public static void main(String[] args) {
         int n = 3;
         TowersHonio(n,"S","H","D");
+        System.out.println("Total moves are hapended");
+        System.out.println((int)Math.pow(2, n)-1);
     }
 }
